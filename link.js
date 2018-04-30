fab0f5ec32a893bce3a9f884cf609c8c8671adf2
H10D5 <38674720+H10D5@users.noreply.github.com>
2018-04-30 06:54:51 +0300

    Add files via upload

---
+++ link.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 5,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 5 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
+
+
+client.login("NDM5Mjg4NzEzMDA5MTY4Mzg2.DcQ_Fw.DD684P8FSVqFx0_nV7PRE22YLgk")
---
+++ start.bat
@@ -0,0 +1,3 @@
+node link.js
+
+pause
